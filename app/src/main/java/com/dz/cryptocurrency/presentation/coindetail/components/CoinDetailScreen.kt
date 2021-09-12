52c62a31a2de6bdf2ea12d608527e55b24aab65d
Fares <mohand@protid.com>
2021-09-12 16:04:31 +0200

    CoinTag TeamListItem

---
+++ app/src/main/java/com/dz/cryptocurrency/presentation/coindetail/components/CoinDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.dz.cryptocurrency.presentation.coindetail.components
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.dz.cryptocurrency.presentation.coindetail.CoinDetailViewModel
+
+@Composable
+fun CoinDetailScreen(
+    navController: NavController,
+    viewModel: CoinDetailViewModel = hiltViewModel()
+) {
+
+}
